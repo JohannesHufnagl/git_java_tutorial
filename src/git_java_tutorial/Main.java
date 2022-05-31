43a071afb98e49a18c9c3b901bd3447c300be5c9
Johannes Hufnagl <johannes.hufnagl4890@gmail.com>
2022-05-31 15:58:49 +0200

    implemented factorial calculation

--- src/git_java_tutorial/Main.java
+++ src/git_java_tutorial/Main.java
@@ -7,5 +7,17 @@ public class Main {
         System.out.println("Hello, git");
         System.out.println("----------");
         System.out.println("Hello SSE");
+
+        final int NUM_FACTS = 100;
+        for (int i = 0; i < NUM_FACTS; i++) {
+            System.out.println(i + "! is " + factorial(i));
+        }
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++)
+            result *= i;
+        return result;
     }
 }
