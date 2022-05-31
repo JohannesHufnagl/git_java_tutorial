775bc16434d3c8969508b0cc5bde777c822402d5
Johannes Hufnagl <johannes.hufnagl4890@gmail.com>
2022-05-31 15:52:00 +0200

    initial commit

---
+++ src/git_java_tutorial/Main.java
@@ -0,0 +1,9 @@
+package git_java_tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, git");
+    }
+}
