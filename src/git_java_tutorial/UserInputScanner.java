15d7c6dc53db6225ac6a2f550a198a0055649212
Johannes Hufnagl <johannes.hufnagl4890@gmail.com>
2022-06-22 10:47:05 +0200

    Added UserInputScanner class for collaboration meeting

---
+++ src/git_java_tutorial/UserInputScanner.java
@@ -0,0 +1,20 @@
+package git_java_tutorial;
+
+import java.util.Scanner;
+
+/**
+ * A class to demonstrate the basic usage of the java Scanner class.
+ *
+ * @author Johannes Hufnagl
+ */
+class UserInputScanner {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myScanner.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+
+        // TODO: Read your birthday as input and print it to the console
+    }
+}
