b123dbd1698ccbc5a1660576be20352ed94a6a30
Noor Mohammed <106658732+noormohammeds@users.noreply.github.com>
2022-06-29 15:29:09 +0530

    finsihed excersise 2

---
+++ src/git_java_tutorial/Noormain.java
@@ -0,0 +1,14 @@
+public class Noormain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x;
+        int y;
+        int sum;
+
+        x = 10;
+        y = 20;
+        sum = x + y;
+        System.out.println(sum);
+    }
+}
+
