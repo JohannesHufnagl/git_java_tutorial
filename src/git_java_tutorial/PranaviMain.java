3aecbba7bb0c48d8de3291c3ff62afe6dfbd68c5
pranavisreenivas <106478880+pranavisreenivas@users.noreply.github.com>
2022-06-29 15:46:11 +0530

    finished exe 4

--- src/git_java_tutorial/PranaviMain.java
+++ src/git_java_tutorial/PranaviMain.java
@@ -1,17 +1,23 @@
 package git_java_tutorial;
 
 import java.util.Scanner;
+
 public class PranaviMain {
 
     public static void main(String[] args) {
-  System.out.println("hello world");
-  int x;
-  int y;
-  int sum;
+        System.out.println("hello world");
+        int x;
+        int y;
+        int sum;
 
-        x = 7;
+        x = 8;
         y = 8;
         sum = x + y;
-    }
 
+        if (sum % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
 }
